8c3ed4d9aab4de77ae651b6f12be105fe0069fdc
lmjing <lmjing@naver.com>
2016-11-17 00:34:38 +0900

    add fcm push

---
+++ app/src/main/java/com/hansjin/mukja_android/FCM/MyFcmListenerService.java
@@ -0,0 +1,82 @@
+package com.hansjin.mukja_android.FCM;
+
+import android.app.Application;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.hansjin.mukja_android.R;
+import com.hansjin.mukja_android.Splash.SplashActivity;
+
+import java.util.Map;
+
+/**
+ * Created by mijeong on 2016. 11. 15..
+ */
+public class MyFcmListenerService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        /*
+        String from = message.getFrom();
+        Map<String, String> data = message.getData();
+        String title = data.get("title");
+        String msg = data.get("message");
+        */
+        RemoteMessage.Notification dd = message.getNotification();
+        String title = dd.getTitle();
+        String msg = dd.getBody();
+
+        sendNotification(title, msg);
+
+        // 전달 받은 정보로 뭔가를 하면 된다.
+        Log.i("fcm","받았다");
+        Log.i("fcm","title : "+title);
+        Log.i("fcm","msg : "+msg);
+    }
+
+    /**
+     * 실제 디바에스에 GCM으로부터 받은 메세지를 알려주는 함수이다. 디바이스 Notification Center에 나타난다.
+     * @param title
+     * @param message
+     */
+    private void sendNotification(String title, String message) {
+        Intent intent = new Intent(this, SplashActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+}
+/*
+public class MyFcmListenerService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        // 이 token을 서버에 전달 한다.
+    }
+}
+*/
---
+++ app/src/main/java/com/hansjin/mukja_android/FCM/MyInstanceIDListenerService.java
@@ -0,0 +1,17 @@
+package com.hansjin.mukja_android.FCM;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by mijeong on 2016. 11. 15..
+ */
+
+public class MyInstanceIDListenerService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        // 이 token을 서버에 전달 한다.
+    }
+}
